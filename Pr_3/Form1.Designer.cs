4243e115743bad062947c17d39fb91c7349dd79a
Емельдяев Антон <aemelia2004@gmail.com>
2024-12-05 11:41:27 +0500

    Вывод таблицы Партнеры

--- Pr_3/Form1.Designer.cs
+++ Pr_3/Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            panel1 = new Panel();
+            TableName = new Label();
+            panel2 = new Panel();
+            TableValue = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(TableName);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Padding = new Padding(20);
+            panel1.Size = new Size(800, 100);
+            panel1.TabIndex = 0;
+            // 
+            // TableName
+            // 
+            TableName.Dock = DockStyle.Fill;
+            TableName.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            TableName.Location = new Point(20, 20);
+            TableName.Name = "TableName";
+            TableName.Size = new Size(760, 60);
+            TableName.TabIndex = 0;
+            TableName.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(TableValue);
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 98);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(800, 352);
+            panel2.TabIndex = 1;
+            // 
+            // TableValue
+            // 
+            TableValue.Dock = DockStyle.Fill;
+            TableValue.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            TableValue.Location = new Point(0, 0);
+            TableValue.Name = "TableValue";
+            TableValue.Size = new Size(800, 352);
+            TableValue.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label TableName;
+        private Panel panel2;
+        private Label TableValue;
     }
 }
--- Pr_3/Form1.cs
+++ Pr_3/Form1.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic.ApplicationServices;
+
 namespace Pr_3
 {
     public partial class Form1 : Form
@@ -5,6 +7,21 @@ namespace Pr_3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SubsystemPartnersContext db = new SubsystemPartnersContext())
+            {
+                var partner = db.Partners.ToList();
+                TableName.Text = "Список партнеров:";
+
+                foreach (Partner u in partner)
+                {
+                    TableValue.Text += $"{u.Id, -5}|    |{u.IdTypePartner, -5}|   |{u.NamePartner, -20}|    |" +
+                        $"{u.LegalAddress, -80}|     |{u.DirectorFullName, -25}|      |{u.Phone, -15}\n";
+                }
+            }
         }
     }
 }
